e1bb8942c8c3ea7c71afde9f16f933db96bbd5c7
Teemu Laulumaa - laptop <teemu.laulumaa@gmail.com>
2018-03-28 14:22:50 +0300

    korjauksia ja paljon juttuja
    action_page:lla on syntaksivirhe :(

--- action_page.php
+++ action_page.php
@@ -4,4 +4,5 @@ include_once(db_connection.php);
 //INSERT INTO `profile` (`name`, `email`, `birthdate`, `gender`) VALUES ('test', 'test@ttt.com', '2018-03-20', '2');
 //https://www.w3schools.com/php/func_mysqli_real_escape_string.asp
 
-$result = db_query("INSERT INTO `profile` (`name`, `email`, `birthdate`, `gender`) VALUES ('" . db_escape_string($_POST["firstname"]) . "', 'test@ttt.com', '2018-03-20', '2')");
+$result = db_query("INSERT INTO profile (name, email, birthdate, gender)
+VALUES (" . db_escape_string($_POST["name"]) . ", " . db_escape_string($_POST["email"]) . ", " . db_escape_string($_POST["birthdate"]) . ", " . db_escape_string($_POST["gender"]) . ");
--- db_connection.php
+++ db_connection.php
@@ -1,19 +1,19 @@
 <?php
 function db_connect() {
 
-    <!--Define connection as a static variable, to avoid connecting more than once -->
+    //Define connection as a static variable, to avoid connecting more than once
     static $connection;
 
-    <!-- Try and connect to the database, if a connection has not been established yet -->
+    //Try and connect to the database, if a connection has not been established yet
     if(!isset($connection)) {
-         <!-- Load configuration as an array. Use the actual location of your configuration file -->
-        $config = parse_ini_file('../config.ini');
+         // Load configuration as an array. Use the actual location of your configuration file
+        $config = parse_ini_file('../../config.ini');
         $connection = mysqli_connect($config['dbaddr'],$config['username'],$config['password'],$config['dbname']);
     }
 
-    <!-- If connection was not successful, handle the error -->
+    // If connection was not successful, handle the error
     if($connection === false) {
-        <!--  Handle error - notify administrator, log to a file, show an error screen, etc.-->
+        //  Handle error - notify administrator, log to a file, show an error screen, etc
         return mysqli_connect_error();
     }
     mysqli_set_charset($connection, "utf8")
