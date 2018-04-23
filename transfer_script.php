727aa776fd780e5ab2b23128e79676581dacbe08
josephbe <36229300+josephbe@users.noreply.github.com>
2018-04-23 14:39:55 +0300

    footerin muokkailua

--- transfer_script.php
+++ transfer_script.php
@@ -1,5 +1,4 @@
 <?php
-include db_connection.php;
 $config = parse_ini_file ('../../config.ini');
 $connection = mysqli_connect($config['dbaddr'],$config['username'],$config['password'],$config['dbname']);
 if($connection === false) {
@@ -40,13 +39,9 @@ var_dump($col_header);
 
 // 1. insert into Session
 // INSERT INTO `Session` (date) VALUES (CURRENT_TIMESTAMP);
-$creating_session = db_query("INSERT INTO Session (date) VALUES (CURRENT_TIMESTAMP);");
 // 2. get the newly created id
 // $seesion_id = mysqli_insert_id($connection)
-$session_id = mysqli_insert_id($connection);
 // 3. insert into Records profile_id from $_SESSION session_id from step 2
-$id = "3";
-$insert_records = db_query("INSERT INTO Records (session_id, profile_id) VALUES ($session_id, $id);");
 
 while (($rows = fgetcsv($fp, 10000, ";")) !== false) {
 	//var_dump($rows);
@@ -54,7 +49,7 @@ while (($rows = fgetcsv($fp, 10000, ";")) !== false) {
 	echo "\n" . $col_header[0] . ": " . $rows[0] . "\n";
 	echo "\n" . $col_header[1] . ": " . $rows[1] . "\n";
 	// 4. insert into RtoR qtc $row[1] session_id $session_id
-  db_query("INSERT INTO `RtoR` (qtc, session_id) VALUES ($rows[1], $session_id);");
+	// INSERT INTO `RtoR` (qtc, session_id) VALUES ($rows[1], $session_id);
 	echo "\n";
 }
 
