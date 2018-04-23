844c6768ea7427d9c25bc12225f83bcc1db4fbe2
Teemu Laulumaa <teemu.laulumaa@gmail.com>
2018-04-23 10:10:49 +0300

    added https and logout phps

---
+++ https.php
@@ -0,0 +1,10 @@
+<?php
+
+
+if(empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] == "off") {
+  $redirect = 'https://' . $_SERVER['HOST'] . $_SERVER['REQUEST_URI'];
+  header('HTTP/1.1 301 Move Permanently');
+  header('Location: ' . $redirect);
+  exit();
+
+}
