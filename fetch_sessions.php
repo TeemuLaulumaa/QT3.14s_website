3776573619bcf6057f676a4497a91bb19651372c
Teemu Laulumaa <teemu.laulumaa@gmail.com>
2018-04-30 12:05:41 +0300

    Filtering data for pieeee

---
+++ fetch_sessions.php
@@ -0,0 +1,55 @@
+<?php
+include_once('db_connection.php');
+
+
+// check which profile the session is on and use data associated with it
+//$curr_session = aasdfasdfasdf
+
+
+$query = "SELECT 'qtc' FROM ((((('RtoR' INNER JOIN 'Session' ON 'RtoR.session_id' = 'Session.session_id') INNER JOIN 'Records' ON 'Session.session_id' = 'Records.session_id') INNER JOIN 'Profile' ON 'Records.profile_id' = 'Profile.profile_id') INNER JOIN 'DiaryDoesSession' ON 'Session.session_id' = 'DiaryDoesSession.session_id') INNER JOIN 'Diary' ON 'DiaryDoesSession.diary_id' = 'Diary.diary_id')";
+
+//Checks what clauses user has set and adds appropriate WHERE clauses.        o = not empty     x = empty
+
+
+//ooo
+if (!empty($_POST['startdate']) && !empty($_POST['enddate']) && !empty($_POST['activity'])) {
+    $query += " WHERE 'Profile.profile_id' = '$curr_session' AND 'Session.date' => ' . db_escape_string($_POST['startdate']) . ' AND 'Session.date' <= ' . db_escape_string($_POST['enddate']) . ' AND 'Diary.activity' == ' . db_escape_string($_POST['activity']) . ';";
+}
+
+//xxx
+if (empty($_POST['startdate']) && empty($_POST['enddate']) && empty($_POST['activity'])) {
+    $query += " WHERE 'Profile.profile_id' = '$curr_session';";
+}
+
+//oxx
+if (!empty($_POST['startdate']) && empty($_POST['enddate']) && empty($_POST['activity'])) {
+    $query += " WHERE 'Profile.profile_id' = '$curr_session' AND 'Session.date' => ' . db_escape_string($_POST['startdate']) . ';";
+}
+
+//oox
+if (!empty($_POST['startdate']) && empty($_POST['enddate']) && !empty($_POST['activity'])) {
+    $query += " WHERE 'Profile.profile_id' = '$curr_session' AND 'Session.date' => ' . db_escape_string($_POST['startdate']) . ';";
+}
+
+//xoo
+if (empty($_POST['startdate']) && !empty($_POST['enddate']) && !empty($_POST['activity'])) {
+    $query += " WHERE 'Profile.profile_id' = '$curr_session' AND 'Session.date' <= ' . db_escape_string($_POST['enddate']) . ' AND 'Diary.activity' == ' . db_escape_string($_POST['activity']) . ';";
+}
+
+//xxo
+if (empty($_POST['startdate']) && empty($_POST['enddate']) && !empty($_POST['activity'])) {
+    $query += " WHERE 'Profile.profile_id' = '$curr_session' AND 'Diary.activity' == ' . db_escape_string($_POST['activity']) . '";
+}
+
+//oxo
+if (!empty($_POST['startdate']) && empty($_POST['enddate']) && !empty($_POST['activity'])) {
+    $query += " WHERE 'Profile.profile_id' = '$curr_session' AND 'Session.date' => ' . db_escape_string($_POST['startdate']) . ' AND 'Diary.activity' == ' . db_escape_string($_POST['activity']) . '";
+}
+
+//xox
+if (empty($_POST['startdate']) && !empty($_POST['enddate']) && empty($_POST['activity'])) {
+    $query += " WHERE 'Profile.profile_id' = '$curr_session' AND 'Session.date' <= ' . db_escape_string($_POST['enddate']) . '";
+}
+
+
+$result = db_query($query);
